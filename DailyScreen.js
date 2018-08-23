d29f2f58ca4e412c13959a7901d699f33a1b9d99
jun2948 <jun1373soccer@gmail.com>
2018-08-23 15:44:42 -0700

    fetched weahter icon

--- DailyScreen.js
+++ DailyScreen.js
@@ -1,54 +1,87 @@
 import React from 'react';
-import {StyleSheet,Text,View ,Button} from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 // import { Constants } from 'expo';
 // import { createStore } from 'redux';
 // import { Provider } from 'react-redux';
 
+const IMAGE_PATH = `http://api.openweathermap.org/img/w/10d.png`;
+
 class DailyScreen extends React.Component {
+  static navigationOptions = ({ navigation }) => ({
+    headerTitle: 'Daily',
+    headerRight: (
+      <Button
+        title="Weekly"
+        onPress={() => navigation.navigate('WeeklyScreen')}
+        color="#3368FF"
+      />
+    ),
+  });
 
-    static navigationOptions = ( {navigation }) => ({
-        headerTitle: 'Daily',
-        headerRight: (
-            <Button
-                title="Weekly"
-                onPress={() => navigation.navigate('WeeklyScreen')}
-                color="#a41034"
-            />
-        ),
-    });
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: [],
+      data: [],
+      weather: [],
+      city: ''
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            dataSource: [],
-            city: '',
-        };
-    }
+  // id cc6fee5f7ba8809d1a0c8f042e9d91f4
 
-    // id cc6fee5f7ba8809d1a0c8f042e9d91f4
+  // fetch OpenWeatherMap API
+  componentDidMount() {
+    return fetch(
+      'https://api.openweathermap.org/data/2.5/weather?q=vancouver&appid=cc6fee5f7ba8809d1a0c8f042e9d91f4&units=metric'
+    )
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState({
+          dataSource: responseJson,
+          data: responseJson.main,
+          weather: responseJson.weather[0]
+        });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
 
-    // fetch OpenWeatherMap API
-    componentDidMount() {
-        return fetch(`https://openweathermap.org/data/2.5/weather?q=${this.state.city}&appid=b6907d289e10d714a6e88b30761fae22`)
-            .then(response => response.json())
-            .then(responseJson => {
-                this.setState({
-                    dataSource: responseJson.weather
-                });
-            })
-            .catch(error => {
-                console.error(error);
-            });
-    }
+  render() {
+    console.log(`city: ${this.state.city}`);
+    return (
+      <View style={styles.container}>
+        <Text style={styles.weatherCondition}>{this.state.dataSource.name}</Text>
+        <Image 
+          style={styles.weatherIcon}
+          source={{ uri: IMAGE_PATH }}
+          />
+        <Text style={styles.temp}>{this.state.data.temp}゜</Text>
+      </View>
+    );
+  }
+}
 
-    render() {
-        console.log(`city: ${this.state.city}`)
-        return (
-            <View>
-                <Text>Hi</Text>
-            </View>
-        );
-    }
-} 
+const styles = StyleSheet.create ({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#f7b733',
+  },
+  weatherCondition: {
+    fontSize: 40,
+    paddingTop: 10,
+    color: '#fff'
+  },
+  weatherIcon: {
+    width: 100,
+    height: 100
+  },
+  temp: {
+    fontSize: 30,
+    color: '#fff'
+  }
+});
 
-export default DailyScreen
+export default DailyScreen;
