aa189426478ea2dd08dc9543b5782a41dddcc4ff
jun2948 <jun1373soccer@gmail.com>
2018-08-24 13:32:39 -0700

    editted redux files

--- redux/reducer.js
+++ redux/reducer.js
@@ -2,16 +2,22 @@ import { combineReducers } from 'redux';
 import { FETCH_WEATHER, SET_CITY } from './action'; 
 
 const fetchWeatherData=(state = {}, action) => {
-  switch(action.type){
-    case FETCH_WEATHER:
-      return {...state, ...action.payload };  
-    default:
-      return state;
+  if (action.type === FETCH_WEATHER){
+    return [...state, ...action.payload ];  
   }
-}
+  return state;
+};
 
+const searchCity =(state = {}, action) => {
+  if (action.type === SET_CITY){
+    return [...state, ...action.payload ];  
+  }
+  return state;
+};
+  
 const reducer = combineReducers({
-
+  weather: fetchWeatherData,
+  city: searchCity
 });
 
 export default reducer;
--- redux/store.js
+++ redux/store.js
@@ -1,6 +1,9 @@
 import { createStore } from 'redux';
+import { weatherData } from './action';
 import reducer from './reducer';
 
 const store = createStore(reducer);
+// store.dispatch(weatherData());
 
+console.log(store.getState());
 export default store;
